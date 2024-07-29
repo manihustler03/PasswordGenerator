2e749d4a60b62d0e9f6d2c66778ff3efe16bf073
arun <arunkannansm@gmail.com>
2024-07-30 02:36:39 +0530

    update: logics added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
 def homePage(request):
-    return render(request,'app/home.html')
+    char = []
+    
+    if request.GET.get('uppercase'):
+        char.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    
+    if request.GET.get('lowercase'):
+        char.extend('abcdefghijklmnopqrstuvwxyz')
+        
+    if request.GET.get('number'):
+        char.extend('0123456789')
+        
+    if request.GET.get('special'):
+        char.extend('\!@#$%^&*()/')
+        
+    try:
+        length = int(request.GET.get('len', 8))
+    except ValueError:
+        length = 8
+
+    if not char:
+        char.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()/')
+
+    gen_password = ''.join(random.choice(char) for i in range(length))
+    
+    
+    return render(request,'app/home.html', {'password':gen_password})
+
